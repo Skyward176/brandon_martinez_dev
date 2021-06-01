8bbf8c5c8d238402ba01f0d23c99b6e24f99df61
Brandon Martinez <brandonam04@outlook.com>
2021-06-01 18:25:13 -0400

    Create footer, add accent color

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,40 +2,93 @@
   <div>
     <a-layout>
       <a-menu v-model="current" mode="horizontal" theme="dark">
-        <a-menu-item key="home" class='name'> Brandon Martinez</a-menu-item>
-        <a-menu-item key="mail"> <a-icon type="profile" />About Me</a-menu-item>
-        <a-menu-item key="app"> <a-icon type="appstore" />My Projects</a-menu-item>
+      <a-menu-item key="home"> <p class='name accent-color'>Brandon Martinez</p></a-menu-item>
+        <a-menu-item key="projects" class='navbar-items'> <font-awesome-icon :icon="['fas', 'project-diagram']"/> &nbsp;Projects</a-menu-item>
+        <a-menu-item key="about" class='navbar-items'> <font-awesome-icon :icon="['fas', 'id-badge']"/> &nbsp;About Me</a-menu-item>
       </a-menu>
-
 
       <a-layout-content>
         <Nuxt/>
       </a-layout-content>
 
 
-      <a-layout-footer>
-        <div class = 'footer'>
-        </div>
+      <a-layout-footer class='navy-background footer'>
+        <a-row>
+          <a-col>
+            <div >
+              <ul>
+                  <li class='footer_item'><a href='#'>Home</a></li>
+                  <li class ='footer_item'><p class='accent-color'>|</p></li>
+                  <li class='footer_item'><a href='#'>Projects</a></li>
+                  <li class='footer_item'><p><span class='accent-color'>|</span> Find me: </p></li>
+                  <li class='footer_item'><p>&nbsp;<font-awesome-icon :icon="['fab', 'github']" class='accent-color'/> Github </p></li>
+                  <li class='footer_item'><p>&nbsp;<font-awesome-icon :icon="['fas', 'envelope-open-text']" class='accent-color'/> eMail </p></li>
+                  <li class='footer_item'><p>&nbsp;<font-awesome-icon :icon="['fas', 'phone-square-alt']" class='accent-color'/> Phone </p></li>
+              </ul>
+            </div>
+          </a-col>
+        </a-row>
+
       </a-layout-footer>
     </a-layout>
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+  export default{}
+</script>
 
-.name {
-  color: orangered;
-  font-size: 1.5rem;
-}
+
+<style>
+  .name {
+    font-size: 1.5rem;
+    padding:0;
+    margin:auto auto auto auto;
+  }
+  .accent-color{
+    color: #5cb4c4;
+  }
+  .navbar-items {
+    font-size: 1.25rem;
+    float:right;
+    display:block;
+  }
+  .navy-background {
+    background-color:#001529; 
+  }
+  .footer {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol','Noto Color Emoji';
+    color: #9cadb4;
+  }
+  li {
+      float: left;
+  }
+
+  a {
+      padding: 8px;
+      font-size:1.5 rem;
+      color:inherit;
+  }
+  a:hover {
+      text-decoration:none;
+      color:inherit;
+  }
+
+  ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+  }
+  .footer_item{
+      color:#9cadb4;;
+      font-size:1.5 rem;
+  }
+  .footer div{
+      display:flex;
+      align-items: middle;
+      justify-content:center;
+  }
+  .contact-text{
+    color: #9cadb4;
+  }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,23 @@
   <div>
     <a-row>
       <a-col>
-        <a-divider><h1> About me </h1></a-divider>
+        <a-divider><h1 class='accent-color'> About me </h1></a-divider>
       </a-col>
     </a-row>
     <a-row>
-      <a-col :span=6>
+      <a-col :span=7>
         <div justify='center' align='center'>
           <img src='/profile_sample.png' width="25%"/>
         </div>
       </a-col>
-      <a-col :span=12>
+      <a-col :span=10>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
       </a-col>
-      <a-col :span=6>
+      <a-col :span=7>
         <div>
           <a-row>
             <a-col :span=6>
-              <font-awesome-icon :icon="['fab', 'js-square']" class='techIcons'/>   
+              <font-awesome-icon :icon="['fab', 'js-square']" class='techIcons accent-color'/>   
             </a-col>
             <a-col :span=18>
               <p> JavaScript</p>
@@ -26,7 +26,7 @@
           </a-row>
           <a-row>
             <a-col :span=6>
-              <font-awesome-icon :icon="['fab', 'js-square']" class='techIcons'/>   
+              <font-awesome-icon :icon="['fab', 'js-square']" class='techIcons accent-color'/>   
             </a-col>
             <a-col :span=18>
               <p> JavaScript</p>
@@ -34,7 +34,7 @@
           </a-row>
           <a-row>
             <a-col :span=6>
-              <font-awesome-icon :icon="['fab', 'js-square']" class='techIcons'/>   
+              <font-awesome-icon :icon="['fab', 'js-square']" class='techIcons accent-color'/>   
             </a-col>
             <a-col :span=18>
               <p> JavaScript</p>
