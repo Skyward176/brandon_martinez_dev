f50e09fd79605b1becd8d33e79c948aef3902a70
Brandon Martinez <brandonam04@outlook.com>
2021-12-04 22:21:29 -0500

    Continue adding nuxt content based project system
    Uses nuxt-context to make a list of projects dynamically based on
    project .json files inputted into the content folder. Started to add an
    ant design carousel, but had issues. Will either troubleshoot or build
    my own carousel component.

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -1,30 +1,37 @@
 <template>
   <div style="background-color: #ececec; padding: 20px;">
+    <div>
+      <a-carousel :dot-position="bottom">
+        <div v-for="project of projects">
+          <h3>{{project.name}}</h3>
+        </div>
+      </a-carousel>
+    </div>
+
     <a-row :gutter="16">
-      <a-col :span="8">
-        <a-card :title=project.name :bordered="false">
-          <p> Project Body: {{ project.description }}</p>
+      <a-col v-for="project of projects" :flex="1">
+        <a-card :title=project.name :bordered="false" >
+          {{project.description}}
         </a-card>
-      </a-col>
-      <a-col :span="8">
-        <a-card title="Card title" :bordered="false">
-          <p>card content</p>
-        </a-card>
-      </a-col>
-      <a-col :span="8">
-        <a-card title="Card title" :bordered="false">
-          <p>card content</p>
-        </a-card>
+        <a-divider/>
       </a-col>
     </a-row>
   </div>
 </template>
+<style scoped>
+  .ant-carousel >>> .slick-slide {
+    text-align: center;
+    background: #364d79;
+  }
+  .ant-carousel >>> .slick-slide h3 {
+    color: #fff;
+  }
+</style>
 <script>
   export default {
     async asyncData({ $content, params }) {
-      const project = await $content('projects', params.slug).fetch()
-
-      return { project }
+      const projects = await $content('projects').fetch()
+      return { projects }
     }
   }
 </script>
