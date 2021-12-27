37adb3cbfed5f272288e320926f6c4c595ec611e
Brandon Martinez <brandonam04@outlook.com>
2021-12-26 23:35:39 -0500

    Fix mobile breakpoints

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,26 @@
 <template>
     <div>
-        <a-row>
-            <a-col>
-                <a-divider><h1 class="accent-color">About me</h1></a-divider>
-            </a-col>
-        </a-row>
-        <a-row :gutter="{ xs: 8, sm: 16, md: 24, lg: 32 }">
-            <a-col :span="7">
-                <div justify="center" align="center">
-                    <Profile />
-                </div>
-            </a-col>
-            <a-col :span="10">
-                <div class="main-text">
-                    <p>
-                        My name is Brandon, and I love to code. I’m trying to
-                        learn all I can, every day. So far, I’ve worked with
-                        Django and Python a lot. I’ve also had the pleasure of
-                        working on websites using HTML/JavaScript/CSS. Recently,
-                        I’ve been experimenting with JavaScript frameworks such
-                        as Vue, React, and NuxtJS. I’ve also done coursework
-                        with Java and played with Rust, C, C++ and Flutter. The
-                        purpose of this site is to document the projects I make
-                        as I continue to learn and do bigger, cooler things with
-                        code. You can see some of the tech I’ve worked with on
-                        the right, or see my contact info below.
-                    </p>
-                </div>
-            </a-col>
-            <a-col :span="7">
+        <a-divider class="title"><h1 class="accent-color">About me</h1></a-divider>
+        <div class="container">
+            <div class="left">
+                <Profile />
+            </div>
+            <div class="middle main-text">
+                <p>
+                    My name is Brandon, and I love to code. I’m trying to
+                    learn all I can, every day. So far, I’ve worked with
+                    Django and Python a lot. I’ve also had the pleasure of
+                    working on websites using HTML/JavaScript/CSS. Recently,
+                    I’ve been experimenting with JavaScript frameworks such
+                    as Vue, React, and NuxtJS. I’ve also done coursework
+                    with Java and played with Rust, C, C++ and Flutter. The
+                    purpose of this site is to document the projects I make
+                    as I continue to learn and do bigger, cooler things with
+                    code. You can see some of the tech I’ve worked with on
+                    the right, or see my contact info below.
+                </p>
+            </div>
+            <div class="right">
                 <a-row type="flex" justify="center">
                     <a-col>
                         <h1 class="accent-color">Technologies</h1>
@@ -101,8 +93,8 @@
                         </div>
                     </a-col>
                 </a-row>
-            </a-col>
-        </a-row>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -126,5 +118,65 @@ h1 {
 .tech-list li {
     justify-content: left;
     display: flex;
+}
+.container {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+}
+.title {
+    width:100%;
+}
+@media (max-width: 599px) {
+    .left{
+        order:1;
+        flex-basis: 80%;
+        margin: auto;
+    }
+    .middle{
+        margin: 2em auto 1em auto;
+        order:2;
+        flex-basis: 80%;
+    }
+    .right{
+        margin: 1em auto 2em auto;
+        order:3;
+        flex-basis: 30%;
+    }
+}
+@media (min-width: 600px) {
+    .left{
+        order:2;
+        flex-basis: 40%;
+        margin:auto;
+    }
+    .middle{
+        margin:auto;
+        order:1;
+        flex-basis: 80%;
+    }
+    .right{
+        margin:auto;
+        order:3;
+        flex-basis: 40%;
+    }
+}
+@media (min-width: 900px) {
+    .left{
+        margin:auto;
+        order:1;
+        flex-basis: 20%;
+    }
+    .middle{
+        margin:auto;
+        order:2;
+        flex-basis: 40%;
+    }
+    .right{
+        margin:auto;
+        order:3;
+        flex-basis: 20%;
+    }
 }
 </style>
