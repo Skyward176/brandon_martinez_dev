2d07114fd7da3f73c8aa3a1bfe8864a28db718f9
Brandon Martinez <brandonam04@outlook.com>
2023-02-14 17:46:38 -0500

    Do some stuff with tailwind
    Very quickly becoming a pain. Gonna find a component library.

---
+++ components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav
+        className="w-full h-1/16 bg-black shadow-lg absolute divide-y divide-gray-50 flex items-center"
+    >
+        <div className=" mx-3 w-7/8">
+            <NuxtLink to="/">
+                <p className="font-light font-sans text-4xl text-gray-50">
+                    Brandon Martinez
+                </p>
+            </NuxtLink>
+        </div>
+        <div className="flex flex-row-reverse w-1/8 align-text-middle">
+            <div className="mx-auto">
+                <NuxtLink to="projects">
+                    <p>Projects</p>
+                </NuxtLink>
+            </div>
+            <div className="w 1/24 ">
+                <NuxtLink to="/">
+                    <p>Projects</p>
+                </NuxtLink>
+            </div>
+        </div>
+    </nav>
+</template>
