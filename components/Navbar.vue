11cf4c91bbfeefa48434f8e7244a098a630056fc
Brandon Martinez <brandonam04@outlook.com>
2023-02-20 13:39:20 -0500

    Fix small navbar details

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,21 +1,23 @@
 <template>
     <div className='w-full flex flex-row h-16 bg-black shadow-lg'>
-        <div className='basis-3/4 w-3/4 my-auto mx-2'>
+        <div className='w-1/2 my-auto mx-2'>
             <NuxtLink href='/'>
                 <p className='font-light font-sans text-4xl text-gray-50'>
-                    Brandon Martinez
+                    Brandon <span className='text-teal-300'>Martinez</span>
                 </p>
             </NuxtLink>
         </div>
-        <div className='basis-1/4 w-1/4 flex flex-row-reverse my-auto '>
-            <NuxtLink className='' to="projects">
-                <p className='font-light font-sans text-xl text-gray-50 mx-2'>
-                    Projects
+        <div className=' w-1/2 flex flex-row justify-end my-auto divide-x divide-gray-50'>
+            <NuxtLink className='flex mx-2 px-2' to="/">
+                <Icon color='white' name='ic:baseline-person' size='28' className='' />
+                <p className='font-light font-sans text-xl text-gray-50 '>
+                    About Me
                 </p>
             </NuxtLink>
-            <NuxtLink className='' to="/">
-                <p className='font-light font-sans text-xl text-gray-50 mx-2'>
-                    About Me
+            <NuxtLink className='flex mx-2 px-2' to="projects">
+                <Icon color='white' name='ph:git-branch-fill' size='28' className='' />
+                <p className='font-light font-sans text-xl text-gray-50'>
+                    Projects
                 </p>
             </NuxtLink>
         </div>
