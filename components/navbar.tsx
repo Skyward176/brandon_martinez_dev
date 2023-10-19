b0c4b6dd908030bd031d71359f9bf8a2c534d8f2
Brandon Martinez <brandonam04@outlook.com>
2023-10-19 17:11:17 -0400

    Add navbar menu, start on active tab colors

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,24 +1,56 @@
 'use client'
 import * as React from 'react';
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
-
-const pages = ['About Me', 'Projects', 'Blog'];
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, NavbarMenu, NavbarMenuItem, NavbarMenuToggle} from "@nextui-org/react";
+import {usePathname} from 'next/navigation';
+const pages = [
+  {name:'About Me', href:'/'}, 
+  {name:'Projects', href:'/projects'},
+  {name: 'Blog', href: '/blog'},
+  {name: 'Resume', href: '/resume'}
+];
 
 function NavbarWrapper() {
-
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const pathname = usePathname()
   return (
-    <Navbar>
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className= 'text-white md:hidden'
+        />
       <NavbarBrand>
-        <p className='text-4xl hidden font-extralight md:block'> Brandon Martinez </p>
-        <p className='text-4xl block font-extralight md:hidden'>BM</p>
+        <p className='text-4xl text-teal-400 font-extralight text-center md:text-left'> Brandon Martinez </p>
       </NavbarBrand>
-      <NavbarContent className='md-hidden'>
+      <NavbarContent justify='end'>
             {pages.map((page) => (
-              <NavbarItem className='bg-black w-full text-center' key={page}>
-                <p className="hover:underline font-extralight text-white text-xl">{page}</p>
+              <NavbarItem key={page.name}>
+                <Link 
+                  href={page.href} 
+                  color={
+                    pathname === page.href ? 'primary':'foreground'
+                  }
+                  className="hover:underline font-extralight text-xl">
+                  {page.name}
+                </Link>
               </NavbarItem>
             ))}
       </NavbarContent>
+      <NavbarMenu>
+          {pages.map((page, index) => (
+            <NavbarMenuItem key={`${page}-${index}`}>
+              <Link 
+                color={
+                  pathname === page.href ? 'primary':'foreground'
+                }
+                href={page.href} 
+                className="font-extralight text-xl w-full"
+                size='lg'
+              >
+                {page.name}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+      </NavbarMenu>
     </Navbar>
   );
 }
