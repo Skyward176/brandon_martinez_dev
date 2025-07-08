b60d447ae43b056254132b9b1fb54f793eebdc30
Brandon Martinez <irism@Brandons-MacBook-Pro.local>
2025-07-08 16:02:30 -0400

    Fix bugs

--- app/blog/components/BlogPost.tsx
+++ app/blog/components/BlogPost.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { useTags } from '@/hooks/useQueries';
 
 interface BlogPostProps {
-  id: string;
+  id?: string;
   title: string;
   content: string;
   createdAt: any;
